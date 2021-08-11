c30abb5436eebb9d56dde9e5cdc8de52f868f895
detachedkite <534872578@qq.com>
2021-08-11 18:40:21 +0800

    [gonghuanhuan] factor : 删除determine amounts for each line，抽取出一个见名思意的方法

--- 6/src/main/java/com/twu/refactoring/Customer.java
+++ 6/src/main/java/com/twu/refactoring/Customer.java
@@ -30,22 +30,7 @@ public class Customer {
 			Rental each = rentals.next();
 
 			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-
-			}
+            thisAmount = this.determineAmountsForEachLine(each);
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -65,6 +50,27 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	private double determineAmountsForEachLine(Rental each){
+		double thisAmount = 0;
+		switch (each.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2)
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3)
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+
+		}
+		return thisAmount;
 	}
 
 }

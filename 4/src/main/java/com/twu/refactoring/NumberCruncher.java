141ff1f1a2d887823f1668c692014e896bff13e8
detachedkite <534872578@qq.com>
2021-08-11 14:59:38 +0800

    [gonghuanhuan] refactor : 使用断言抽取四个方法的共同代码

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,27 +1,39 @@
 package com.twu.refactoring;
 
 import java.util.Arrays;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 
 public class NumberCruncher {
     private final int[] numbers;
+    private IntPredicate intPredicate;
 
     public NumberCruncher(int... numbers) {
         this.numbers = numbers;
     }
 
     public int countEven() {
-        return (int)Arrays.stream(numbers).filter(number -> number%2 == 0).count();
+        intPredicate = number -> number%2 == 0;
+        return filter(intPredicate);
     }
 
     public int countOdd() {
-        return (int)Arrays.stream(numbers).filter(number -> number%2 == 1).count();
+        intPredicate = number -> number%2 ==1;
+        return filter(intPredicate);
     }
 
     public int countPositive() {
-        return (int)Arrays.stream(numbers).filter(number -> number >= 0).count();
+        intPredicate = number -> number >= 0;
+        return filter(intPredicate);
     }
 
     public int countNegative() {
-        return (int)Arrays.stream(numbers).filter(number -> number < 0).count();
+        intPredicate = number -> number < 0;
+        return filter(intPredicate);
     }
+
+    private int filter(IntPredicate predicate){
+        return (int)Arrays.stream(numbers).filter(predicate).count();
+    }
+
 }

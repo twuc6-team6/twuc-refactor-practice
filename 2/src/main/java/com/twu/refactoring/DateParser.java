f41784b3210bc1dc346bc37c47786a36378caca0
detachedkite <534872578@qq.com>
2021-08-15 19:00:38 +0800

    [gonghuanhuan] refactor:取消year date等成员变量，变成局部变量

--- 2/src/main/java/com/twu/refactoring/DateParser.java
+++ 2/src/main/java/com/twu/refactoring/DateParser.java
@@ -8,11 +8,6 @@ import java.util.TimeZone;
 public class DateParser {
     private final String dateAndTimeString;
     private static final HashMap<String, TimeZone> KNOWN_TIME_ZONES = new HashMap<String, TimeZone>();
-    private int year;
-    private int month;
-    private int date;
-    private int hour;
-    private int minute;
 
     static {
         KNOWN_TIME_ZONES.put("UTC", TimeZone.getTimeZone("UTC"));
@@ -32,11 +27,11 @@ public class DateParser {
     }
 
     public Date parse() {
-        initYear();
-        initMonth();
-        initDay();
-        initHour();
-        initMinute();
+        int year = initYear();
+        int month = initMonth();
+        int date = initDay();
+        int hour = initHour();
+        int minute = initMinute();
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -45,64 +40,36 @@ public class DateParser {
         return calendar.getTime();
     }
 
-    private void initHour() {
+    private int initHour() {
+        int hour;
         if (dateAndTimeString.substring(11, 12).equals("Z")) {
             hour = 0;
         } else {
-            try {
-                hour = getTimeComponentFromStringToInt(11,13);
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new IllegalArgumentException("Hour string is less than 2 characters");
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Hour is not an integer");
-            }
-            if (hour < 0 || hour > 23)
-                throw new IllegalArgumentException("Hour cannot be less than 0 or more than 23");
+            hour = getTimeComponent(11,13,"Hour",0,23,2);
         }
+        return hour;
     }
 
-    private void initDay() {
-        try {
-            date = getTimeComponentFromStringToInt(8,10);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Date string is less than 2 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Date is not an integer");
-        }
-        if (date < 1 || date > 31)
-            throw new IllegalArgumentException("Date cannot be less than 1 or more than 31");
+    private int initDay() {
+        return getTimeComponent(8,10,"Date",1,31,2);
     }
 
-    private void initYear() {
-        try {
-            year = getTimeComponentFromStringToInt(0,4);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Year string is less than 4 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Year is not an integer");
-        }
-        if (year < 2000 || year > 2012)
-            throw new IllegalArgumentException("Year cannot be less than 2000 or more than 2012");
+    private int initYear() {
+        return getTimeComponent(0,4,"Year",2000,2012,4);
     }
 
-    private void initMonth() {
-        try {
-            month = getTimeComponentFromStringToInt(5,7);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Month string is less than 2 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Month is not an integer");
-        }
-        if (month < 1 || month > 12)
-            throw new IllegalArgumentException("Month cannot be less than 1 or more than 12");
+    private int initMonth() {
+        return getTimeComponent(5,7,"Month",1,12,2);
     }
 
-    private void initMinute() {
+    private int initMinute() {
+        int minute;
         if (dateAndTimeString.substring(11, 12).equals("Z")) {
             minute = 0;
         } else {
-            exceptionHandler(11,12,"Minute",0,59);
+            minute = getTimeComponent(14,16,"Minute",0,59,2);
         }
+        return minute;
     }
 
     private int getTimeComponentFromStringToInt(int start,int end){
@@ -110,15 +77,17 @@ public class DateParser {
         return Integer.parseInt(dateComponentString);
     }
 
-    private void exceptionHandler(int stat,int end,String timeComponent,int min,int max){
+    private int getTimeComponent(int start,int end,String timeComponent,int min,int max,int length){
+        int time = 0;
         try {
-            minute = getTimeComponentFromStringToInt(14,16);
+            time = getTimeComponentFromStringToInt(start,end);
         } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException(timeComponent+" string is less than 2 characters");
+            throw new IllegalArgumentException(timeComponent+" string is less than "+length+" characters");
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(timeComponent+" is not an integer");
         }
-        if (minute < min || minute > max)
+        if (time < min || time > max)
             throw new IllegalArgumentException(timeComponent+" cannot be less than "+min+" or more than "+max);
+        return time;
     }
 }
